e60a7660c9307f34797d984b854d28adc1db1f6d
Inmay <aokatta@163.com>
2017-05-09 12:50:16 +0800

    first commit

---
+++ myNote/myNote/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  myNote
+//
+//  Created by WuYueFeng on 2017/4/20.
+//  Copyright © 2017年 WuYueFeng. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
